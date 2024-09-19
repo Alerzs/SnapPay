959c0cbd2c62c6432b2389e09aa6e0c672a587f3
Alireza Shams <alerzs1023@gmail.com>
2024-09-19 11:14:57 +0330

    allow host

--- snapfood/snapfood/settings.py
+++ snapfood/snapfood/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(',')
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(',')
+#ALLOWED_HOSTS = []
 
 # Application definition
 
